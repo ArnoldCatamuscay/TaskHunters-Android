a81a633876b5ff53b4260edb9da6192c0c955a9d
Cristian <cristianenv13@gmail.com>
2024-03-15 19:04:21 -0500

    View Register

--- app/src/main/java/co/unicauca/taskhunters/MainActivity.kt
+++ app/src/main/java/co/unicauca/taskhunters/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,26 +22,23 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //Greeting("Android")
-                    //RegisterForm()
-                    Home()
+                    RegisterForm()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     TaskHuntersTheme {
-        Greeting(name = "Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            //Greeting("Android")
+            RegisterForm()
+        }
     }
 }
